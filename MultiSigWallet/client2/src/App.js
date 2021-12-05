9be21c1c2440e9cba4aab57a2a64d994427773f7
derZeitgeist@zeitgeist.com <andy@Andrejs-MacBook-Air.local>
2021-12-05 14:13:58 +0100

    Working now

--- MultiSigWallet/client2/src/App.js
+++ MultiSigWallet/client2/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react"
+import {getWeb3, getWallet} from "./utils.js"
 
 function App() {
+  const [web3, setWeb3] = useState(undefined)
+  const [accounts, setAccounts] = useState(undefined)
+  const [wallet, setWallet] = useState(undefined)
+  const [approvers, setApprovers] = useState(undefined)
+  const [quorum, setQuorum] = useState(undefined)
+
+  // #later: difference between useEffect and useState
+  useEffect( () =>{
+    const init = async () => {
+      const web3 = getWeb3();
+      const accounts = await web3.eth.getAccounts()
+      const wallet = await getWallet(web3)
+      const approvers = await wallet.methods.getApprovers().call()
+      const quorum = await wallet.methods.quorum().call()
+      setWeb3(web3);
+      setAccounts(accounts)
+      setWallet(wallet)
+      setApprovers(approvers)
+      setQuorum(quorum)
+
+    };
+    init();
+  },[])
+
+  if (
+    typeof web3 === "undefined" 
+    || typeof accounts === "undefined"
+    // || typeof wallet === "undefined"
+    // //|| typeof approvers === "undefined"
+    // //|| typeof quorum === "undefined"
+    ) 
+  {return <div> Loading...</div>}
+  
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      MultiSigWallet
+      <p>Approvers: {approvers}}</p>
     </div>
   );
 }
